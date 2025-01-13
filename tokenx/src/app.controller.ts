db538758d78f1b6905f32b464e3089a453a60264
SachPlayZ <ssachinsingh99@gmail.com>
2025-01-14 00:58:18 +0530

    Made the property api

--- tokenx/src/app.controller.ts
+++ tokenx/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PropertyDto } from './dto/propertyDto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("property")
+  addProperty(@Body() data: PropertyDto) {
+    return this.appService.addProperty(data);
+  }
+
+  @Get("property")
+  getAllProperties() {
+    return this.appService.getAllProperties();
+  }
+
+  @Get("property/:id")
+  getPropertyById(id: number) {
+    return this.appService.getPropertyById(id);
+  }
+
+  @Patch("property/:id")
+  updateProperty(id: number, @Body() data: PropertyDto) {
+    return this.appService.updateProperty(id, data);
+  }
+
+  @Delete("property/:id")
+  deleteProperty(id: number) {
+    return this.appService.deleteProperty(id);
   }
 }
---
+++ tokenx/src/dto/propertyDto.ts
@@ -0,0 +1,8 @@
+export class PropertyDto {
+    owner : string;
+    name : string;
+    location : string;
+    price : number;
+    bedrooms : number;
+    sqft : number;
+}
