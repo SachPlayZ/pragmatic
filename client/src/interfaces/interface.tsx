6f200350e48ceb564291592b8232dbaf549ab876
Srijan Sarkar <srijansarkar2705@gmail.com>
2025-01-13 22:42:30 +0530

    update interface

--- client/src/interfaces/interface.tsx
+++ client/src/interfaces/interface.tsx
@@ -1,13 +1,10 @@
 export interface Property {
-  id: string;
   name: string;
   location: string;
   price: number;
-  tokenPrice: number;
   totalTokens: number;
   availableTokens: number;
   imageUrl: string;
   bedrooms: number;
-  bathrooms: number;
   sqft: number;
 }
