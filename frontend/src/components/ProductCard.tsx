9481b0c6ebd8e401a258fbb368380f229d56fbbe
Srijan Sarkar <srijansarkar2705@gmail.com>
2025-01-18 18:13:54 +0530

    added buying

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,5 +1,9 @@
+import { contractAbi, contractAddress } from "@/abi";
 import { motion } from "framer-motion";
 import { Home, Maximize2 } from "lucide-react";
+import { useState } from "react";
+import { parseEther } from "viem";
+import { useWriteContract } from "wagmi";
 
 interface Property {
   id: number;
@@ -16,6 +20,50 @@ interface PropertyCardProps {
 }
 
 export default function PropertyCard({ property }: PropertyCardProps) {
+  const { writeContractAsync } = useWriteContract();
+  const [transactionStatus, setTransactionStatus] = useState("");
+  const [transactionHash, setTransactionHash] = useState("");
+
+  async function handleBuy(id: number) {
+    console.log(`Buying property with id: ${id}`);
+    try {
+      const tx = await writeContractAsync(
+        {
+          address: contractAddress,
+          abi: contractAbi,
+          functionName: "buyProperty",
+          args: [id],
+          value: parseEther(property.salePrice.toString()),
+        },
+        {
+          onSuccess(data: any) {
+            console.log("Transaction successful!", data);
+            setTransactionStatus("Transaction submitted!");
+            setTransactionHash(data?.hash);
+          },
+          onSettled(data: any, error: any) {
+            if (error) {
+              setTransactionStatus("Transaction failed.");
+              console.error("Error on settlement:", error);
+            } else {
+              console.log("Transaction settled:", data);
+              setTransactionStatus("Transaction confirmed!");
+              setTransactionHash(data?.hash);
+            }
+          },
+        }
+      );
+      if (tx) {
+        console.log("Transaction hash:", tx);
+        setTransactionHash(tx);
+        setTransactionStatus("Transaction confirmed!");
+      }
+    } catch (error) {
+      console.error("Error submitting transaction:", error);
+      setTransactionStatus("Transaction failed.");
+    }
+  }
+
   return (
     <div className="backdrop-blur-lg bg-white/10 rounded-2xl overflow-hidden border border-white/20 w-full h-[220px] flex flex-col sm:flex-row">
       <div className="w-full sm:w-1/2 h-1/2 sm:h-full overflow-hidden">
@@ -54,6 +102,7 @@ export default function PropertyCard({ property }: PropertyCardProps) {
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
               className="flex-1 bg-gradient-to-r from-[#D0FD3E] to-[#9EF01A] text-[#0A1A1F] px-2 py-1 rounded-lg font-medium hover:shadow-lg hover:shadow-[#D0FD3E]/20 transition-all text-center text-sm flex items-center justify-center"
+              onClick={() => handleBuy(property.id)}
             >
               Buy: {property.salePrice}{" "}
               <img src="/avax_black.svg" alt="coin" className="w-4 h-4 ml-1" />
